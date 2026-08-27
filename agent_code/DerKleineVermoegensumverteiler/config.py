853511efbbe6d5c31ed62efafff6f165329204b1
LiliWestermann <lili.westermann@stud.uni-heidelberg.de>
2026-08-27 17:02:07 +0200

    fix(agent): accidentally pushed wrong version before

--- agent_code/DerKleineVermoegensumverteiler/config.py
+++ agent_code/DerKleineVermoegensumverteiler/config.py
@@ -18,10 +18,10 @@ MINIMUM_EPSILON: float = 0.1
 # rewards
 REWARDS: dict[str, float] = {
     "INVALID_ACTION": -0.5,
-    "WAITED": -0.3,
+    "WAITED": -0.1,
     "COIN_COLLECTED": 10.0,
-    "MOVED_TOWARDS_COIN": 1.0,
-    "MOVED_AWAY_FROM_COIN": -1.0,
+    # "MOVED_TOWARDS_COIN": 1.0,
+    # "MOVED_AWAY_FROM_COIN": -1.0,
 }
 
 # seeds
